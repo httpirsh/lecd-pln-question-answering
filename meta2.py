85be7fa78465e81fef19ef55b31d736bc425f9af
EduCarddoso <eduardosncardoso@gmail.com>
2023-11-22 01:01:57 +0000

    Treino dos modelos com os dados de treino

--- meta2.py
+++ meta2.py
@@ -27,22 +27,24 @@ def get_word2vec_embeddings(model, text):
 def prepare_data(conv, quest, options):
 
     # TF-IDF
-    tfidf_vectorizer = TfidfVectorizer(strip_accents='unicode')
-    tfidf_vectorizer.fit([conv] + [quest] + options)
-    conv_tfidf = tfidf_vectorizer.transform([conv])
+    tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2), strip_accents='unicode')
+    tfidf_vectorizer.fit(conv + [quest] + options)
+    conv_tfidf = [tfidf_vectorizer.transform([sentence]) for sentence in conv]
     quest_tfidf = tfidf_vectorizer.transform([quest])
-    options_tfidf = tfidf_vectorizer.transform(options)
+    options_tfidf = [tfidf_vectorizer.transform([option]) for option in options]
 
     # Word2Vec
-    conv_w2v = get_word2vec_embeddings(word2vec_model, conv)
+    conv_w2v = np.array([get_word2vec_embeddings(word2vec_model, sentence) for sentence in conv])
     quest_w2v = get_word2vec_embeddings(word2vec_model, quest)
     options_w2v = np.array([get_word2vec_embeddings(word2vec_model, opt) for opt in options])
 
     return conv_tfidf, quest_tfidf, options_tfidf, conv_w2v, quest_w2v, options_w2v
+
 # ---------------------------------------------------------------------------
 
 # Pré-processamento dos dados -> lematização, remover potuação, etc.
-def preprocessing(conv, quest, options):
+# Aplicar à resposta correta para conseguir comparar com a resposta prevista pelos vários métodos
+def preprocessing(conv, quest, options, answer):
 
     # Escolher apenas as falas do diálogo correspondentes ao sujeito principal da pergunta
     dep_question = {}
@@ -87,15 +89,20 @@ def preprocessing(conv, quest, options):
             option_lemmas.append(opt_token.lemma_)
         opt_lemmas.append(option_lemmas)
 
+    ans_lemmas = []
+    for ans_token in nlp(answer):
+        ans_lemmas.append(ans_token.lemma_)
+
     # Remover pontuação
     conv = [remove_punct(sent) for sent in conv_lemmas]
     quest = " ".join(remove_punct(quest_lemmas))
     options = [" ".join(remove_punct(opt)) for opt in opt_lemmas]
+    ans = " ".join(remove_punct(ans_lemmas))
 
     # Remover stopwords
     conv = [" ".join(remove_stop(sent)) for sent in conv]
 
-    return conv, quest, options
+    return conv, quest, options, ans
 
 # Remover pontuação de uma lista de palavras
 def remove_punct(text):
@@ -120,110 +127,146 @@ def remove_stop(text):
 
     return new_text
 
+# Dados de treino para os modelos
+def train_data():
+    X_tfidf = []
+    X_w2v = []
+    y = []
+
+    all_text = []
+
+    text = []
+    questions = []
+    opt = []
+    ans = []
+
+    with open('train.json', 'r') as file:
+        data = json.load(file)
+    
+    n = 1
+    for conversation_data in data:
+        for qa_pair in conversation_data[1]:
+            
+            conversation_text = conversation_data[0]
+
+            question = qa_pair['question'].lower()
+            choices = [qa_pair['choice'][i].lower() for i in range(len(qa_pair['choice']))]
+            answer = qa_pair['answer'].lower()
+
+            # Realizar pré-processamento nos dados
+            conversation_text, question, options, answer = preprocessing(conversation_text, question, choices, answer)
+            text.append(conversation_text)
+            questions.append(question)
+            opt.append(options)
+            ans.append(answer)
+
+            all_text.extend(conversation_text)
+            all_text.append(question)
+            all_text.extend(options)
+
+        n += 1
+        if n > 500:
+            break
+
+    tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2), strip_accents='unicode')
+    tfidf_vectorizer.fit(all_text)
+
+    n=1
+    for c, q, o, a in zip(text, questions, opt, ans):
+
+        for choice in o:
+            c.append(q)
+            c.append(choice)
+            all = " ".join(c)
+            all_tfidf = tfidf_vectorizer.transform([all])
+            all_w2v = get_word2vec_embeddings(word2vec_model, all)
+
+            X_tfidf.append([all_tfidf])
+            X_w2v.append(all_w2v)
+            y.append(1 if choice == a else 0)           
+
+        n += 1
+        if n > 2:
+            break
+
+    return np.array(X_tfidf), np.array(X_w2v), np.array(y)
+
 # ---------------------------------------------------------------------------
 
 # Topic Modelling
-def topic_modelling(conv, quest, options, options_w2v):
+def topic_modelling(conv, quest, options):
     from sklearn.decomposition import LatentDirichletAllocation
 
+    conv_tfidf, quest_tfidf, options_tfidf, conv_w2v, quest_w2v, options_w2v = prepare_data(conv, quest, options)
+
     # TF-IDF
-    tf_vect = TfidfVectorizer(ngram_range=(1, 3), strip_accents='unicode')
-    tf_vect.fit([conv])
-
-    sentences = nltk.sent_tokenize(conv)
-    conv_vect = [tf_vect.transform([sentence]) for sentence in sentences]
-    quest_vect = tf_vect.transform([question])
-    opt_vect = [tf_vect.transform([options[i]]) for i in range(len(options))]
-
     lda = LatentDirichletAllocation(n_components=2, doc_topic_prior=0.5, topic_word_prior=0.5)
 
     best = []
-    for i in range(len(sentences)):
-        lda.fit(conv_vect[i])
-        quest_topics = lda.transform(quest_vect)[0]
+    for i in range(len(conv_tfidf)):
+        lda.fit(conv_tfidf[i])
+        quest_topics = lda.transform(quest_tfidf)[0]
         best.append(np.sum(quest_topics))
 
-    best_sentence = conv_vect[np.argmax(best)]
+    best_sentence = conv_tfidf[np.argmax(best)]
 
     lda.fit_transform(best_sentence)[0]
-    opt_topics = [lda.transform(opt_vect[i])[0] for i in range(len(opt_vect))]
+    opt_topics = [lda.transform(options_tfidf[i])[0] for i in range(len(options_tfidf))]
 
-    similarity = [np.sum(opt_topics[i]) + 0.4*cosine_similarity(best_sentence, opt_vect[i])
-                  for i in range(len(opt_vect))]
+    similarity = [np.sum(opt_topics[i]) + 0.4*cosine_similarity(best_sentence, options_tfidf[i])
+                  for i in range(len(options_tfidf))]
     correct_option_tfidf = options[np.argmax(similarity)]
 
     # Usar similaridade de cosseno para Word2Vec (O LDA não é aplicado diretamente a embeddings densos como Word2Vec)
-    quest_w2v = get_word2vec_embeddings(word2vec_model, quest)
-    sent_w2v = np.array([get_word2vec_embeddings(word2vec_model, sentence) for sentence in sentences])
-
     best = []
-    for i in range(len(sentences)):
-        sim = cosine_similarity([sent_w2v[i]], [quest_w2v])[0][0]
+    for i in range(len(conv_w2v)):
+        sim = cosine_similarity([conv_w2v[i]], [quest_w2v])[0][0]
         best.append(sim)
 
-    best_sentence = sent_w2v[np.argmax(best)]
+    best_sentence = conv_w2v[np.argmax(best)]
 
-    similarity = [cosine_similarity([best_sentence], [opt_emb])[0][0] for opt_emb in options_w2v]
+    similarity = [cosine_similarity([best_sentence], [opt])[0][0] for opt in options_w2v]
     correct_option_w2v = options[np.argmax(similarity)]
 
     return correct_option_tfidf, correct_option_w2v
 
 
 # Support Vector Machines - classificação binária
-def svm_prediction(conv, options):
-    from sklearn.svm import SVC
-
-    conv_tfidf, quest_tfidf, options_tfidf, conv_w2v, quest_w2v, options_w2v = prepare_data(conv, options)
+def svm_prediction(model_tf, model_w2v, x_tf, x_w2v, options):
 
     # SVM com TF-IDF
-    svm_model_tfidf = SVC(kernel='linear')
-    svm_model_tfidf.fit(conv_tfidf, [0])
-    predictions_tfidf = svm_model_tfidf.predict(options_tfidf)
+    predictions_tfidf = model_tf.predict(x_tf)
     pred_option_tfidf = options[np.argmax(predictions_tfidf)]
 
     # SVM com Word2Vec
-    svm_model_w2v = SVC(kernel='linear')
-    svm_model_w2v.fit([conv_w2v], [0])
-    predictions_w2v = svm_model_w2v.predict(options_w2v)
+    predictions_w2v = model_w2v.predict(x_w2v)
     pred_option_w2v = options[np.argmax(predictions_w2v)]
 
     return pred_option_tfidf, pred_option_w2v
 
 
 # Árvore de Decisão
-def tree_prediction_both(conv, options):
-    from sklearn.tree import DecisionTreeClassifier
-
-    conv_tfidf, quest_tfidf, options_tfidf, conv_w2v, quest_w2v, options_w2v = prepare_data(conv, options)
+def tree_prediction_both(model_tf, model_w2v, x_tf, x_w2v, options):
 
     # Árvore de Decisão com TF-IDF
-    tree_model_tfidf = DecisionTreeClassifier()
-    tree_model_tfidf.fit(conv_tfidf, [0])
-    predictions_tfidf = tree_model_tfidf.predict(options_tfidf)
+    predictions_tfidf = model_tf.predict(x_tf)
     pred_option_tfidf = options[np.argmax(predictions_tfidf)]
 
     # Árvore de Decisão com Word2Vec
-    tree_model_w2v = DecisionTreeClassifier()
-    tree_model_w2v.fit([conv_w2v], [0])
-    predictions_w2v = tree_model_w2v.predict(options_w2v)
+    predictions_w2v = model_w2v.predict(x_w2v)
     pred_option_w2v = options[np.argmax(predictions_w2v)]
 
     return pred_option_tfidf, pred_option_w2v
 
-def naive_bayes(conv, options):
-    from sklearn.naive_bayes import GaussianNB
-    model = GaussianNB()
 
-    conv_tfidf, quest_tfidf, options_tfidf, conv_w2v, quest_w2v, options_w2v = prepare_data(conv, options)
+def naive_bayes(model_tf, model_w2v, x_tf, x_w2v, options):
 
     # Naive Bayes com TF-IDF
-    model.fit(conv_tfidf, [0])
-    predictions_tfidf = model.predict(options_tfidf)
+    predictions_tfidf = model_tf.predict(x_tf)
     pred_option_tfidf = options[np.argmax(predictions_tfidf)]
 
     # Naive Bayes com Word2Vec
-    model.fit([conv_w2v], [0])
-    predictions_w2v = model.predict(options_w2v)
+    predictions_w2v = model_w2v.predict(x_w2v)
     pred_option_w2v = options[np.argmax(predictions_w2v)]
 
     return pred_option_tfidf, pred_option_w2v
@@ -284,6 +327,32 @@ def calculate_sas(true_answer, predicted_answer, model_name="sentence-transforme
     return sas_score
 
 # ---------------------------------------------------------------------------
+
+X_tfidf, X_w2v, y = train_data()
+
+# Treino dos modelos
+from sklearn.svm import SVC
+svm_model_tfidf = SVC(kernel='linear')
+svm_model_tfidf.fit(X_tfidf, y)
+svm_model_w2v = SVC(kernel='linear')
+svm_model_w2v.fit(X_w2v, y)
+
+from sklearn.tree import DecisionTreeClassifier
+tree_model_tfidf = DecisionTreeClassifier()
+tree_model_tfidf.fit(X_tfidf, y)
+tree_model_w2v = DecisionTreeClassifier()
+tree_model_w2v.fit(X_w2v, y)
+
+from sklearn.naive_bayes import GaussianNB
+model_tfidf = GaussianNB()
+model_tfidf.fit(X_tfidf, y)
+model_w2v = GaussianNB()
+model_w2v.fit(X_w2v, y)
+
+tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 2), strip_accents='unicode')
+
+# ---------------------------------------------------------------------------
+
 with open('test.json', 'r') as file:
     data = json.load(file)
 
@@ -307,18 +376,30 @@ for conversation_data in data:
         answer = qa_pair['answer'].lower()
 
         # Realizar pré-processamento nos dados
-        conversation_text, question, choices = preprocessing(conversation_text, question, choices)
+        conversation_text, question, choices, answer = preprocessing(conversation_text, question, choices, answer)
 
-        print(conversation_text, "\n", question, "\n", choices)
+        # Organizar dados para serem classificados pelos modelos
+        Xt_tfidf, Xt_w2v, yt = [], [], []
+        for choice in choices:
 
-        # Preparar os dados para as representações
-        conv_tfidf, options_tfidf, conv_w2v, options_w2v = prepare_data(conversation_text, choices)
+            conversation_text.append(question)
+            conversation_data.append(choice)
+            all = " ".join(conversation_text)
+
+            tfidf_vectorizer.fit([all])
+
+            all_tfidf = tfidf_vectorizer.transform([all])
+            all_w2v = get_word2vec_embeddings(word2vec_model, all)
+
+            Xt_tfidf.append([all_tfidf])
+            Xt_w2v.append(all_w2v)
+            yt.append(1 if choice == answer else 0)    
 
         # Chamar as funções de previsão para cada modelo
-        pred_svm_tfidf, pred_svm_w2v = svm_prediction(conversation_text, choices)
-        pred_tree_tfidf, pred_tree_w2v = tree_prediction_both(conversation_text, choices)
-        pred_topic_tfidf, pred_topic_w2v = topic_modelling(conversation_text, question, choices, options_w2v)
-        pred_nb_tfidf, pred_nb_w2v = naive_bayes(conversation_text, choices)
+        pred_svm_tfidf, pred_svm_w2v = svm_prediction(svm_model_tfidf, svm_model_w2v, Xt_tfidf, Xt_w2v, choices)
+        pred_tree_tfidf, pred_tree_w2v = tree_prediction_both(tree_model_tfidf, tree_model_w2v, Xt_tfidf, Xt_w2v, choices)
+        pred_topic_tfidf, pred_topic_w2v = topic_modelling(conversation_text, question, choices)
+        pred_nb_tfidf, pred_nb_w2v = naive_bayes(model_tfidf, model_w2v, Xt_tfidf, Xt_w2v, choices)
 
         # Armazenar as respostas verdadeiras e as previstas
         true_answers.append(answer)
@@ -330,8 +411,6 @@ for conversation_data in data:
         predicted_answers_topic_w2v.append(pred_topic_w2v)
         predicted_answers_nb_tfidf.append(pred_nb_tfidf)
         predicted_answers_nb_w2v.append(pred_nb_w2v)
-
-    break
 
 print('SVM: TF-IDF ----------------------------')
 evaluation_metrics(true_answers, predicted_answers_svm_tfidf)
